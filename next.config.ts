317104a9265028e7ea4f9c8edb226e1e073cccfc
infix123 <adediran.oludare@gmail.com>
2025-09-03 21:40:20 -0400

    updated

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	webpack: (config, _) => ({
+		...config,
+		watchOptions: {
+			...config.watchOptions,
+			poll: 800,
+			aggregateTimeout: 300,
+		},
+	}),
 };
 
 export default nextConfig;
